bb4ef75800511d4dce95dc3ea6147a75f5de7646
Carlos David <dave_u@outlook.com>
2023-01-03 13:45:18 -0600

    se intenta arreglar el logo

--- script/index.js
+++ script/index.js
@@ -1,13 +1,24 @@
 
 function encriptar() {
     var text = document.getElementById('text-input').value;
+    var letras = "abcdefghijklmnñopqrstuvwxyz";
+    var desplazamiento = 1;
+
     if (text === '') {
-        alert("Ingresa un texto")
+        alert("Introduce un texto")
         return
     } else {
-        document.getElementById("text-input").innerHTML = " ";
+        for (let i = 0; i<text.length; i++){
+            if(letras.indexOf(texto[i])!=-1){
+                let posicion = ((letras.indexOf(texto[i])+desplazamiento))
+                resultado += letras[posicion];
+                document.getElementById("text-input").innerHTML = " ";
         document.getElementById("text-header").innerHTML = " ";
-        document.getElementById("text-out").innerHTML = text;  
+        document.getElementById("text-out").innerHTML = resultado;
+            }
+        }
+        
+          
     }
     
     
