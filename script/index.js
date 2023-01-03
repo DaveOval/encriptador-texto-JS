9d208b212e98534e4773ab2aec91a5b64e7dec24
Carlos David <dave_u@outlook.com>
2023-01-03 14:51:07 -0600

    se empieza el js

--- script/index.js
+++ script/index.js
@@ -1,25 +1,28 @@
-
-function encriptar() {
+    var encrypted = "";
     var text = document.getElementById('text-input').value;
     var letras = "abcdefghijklmnñopqrstuvwxyz";
     var desplazamiento = 1;
 
-    if (text === '') {
-        alert("Introduce un texto")
-        return
-    } else {
-        for (let i = 0; i<text.length; i++){
-            if(letras.indexOf(texto[i])!=-1){
-                let posicion = ((letras.indexOf(texto[i])+desplazamiento))
-                resultado += letras[posicion];
-                document.getElementById("text-input").innerHTML = " ";
-        document.getElementById("text-header").innerHTML = " ";
-        document.getElementById("text-out").innerHTML = resultado;
-            }
-        }
-        
-          
+function encriptar(text, desplazamiento) {
+    var encrypted = "";
+  for (var i = 0; i < text.length; i++) {
+    var letter = text[i];
+    // Si es una letra, se encripta
+    if (letter.match(/[a-z]/i)) {
+      var code = text.charCodeAt(i);
+      // Se convierte a mayúsculas
+      if ((code >= 65) && (code <= 90)) {
+        letter = String.fromCharCode(((code - 65 + desplazamiento) % 26) + 65);
+      }
+      // Se convierte a minúsculas
+      else if ((code >= 97) && (code <= 122)) {
+        letter = String.fromCharCode(((code - 97 + desplazamiento) % 26) + 97);
+      }
+      console.log(encrypted)
     }
+    encrypted += letter;
+  }
+  return encrypted;
     
     
 }
